ed9a562faeb153bf396c55fc85de8d1c27d9710e
Mariana <marianapertuz@uninorte.edu.co>
2020-06-09 18:37:15 -0500

    con home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,23 +1,229 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import app from "firebase/app";
-import { withRouter } from 'react-router-dom'
+import { withRouter } from "react-router-dom";
 import Navigation from "../NavBar/Navigation";
 import * as ROUTES from "../Routes/Routes";
 import "firebase/database";
-import './Home.css'
+import "./Home.css";
 
-var firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/database');
+import _ from "lodash";
+import { Chart } from "react-google-charts";
+import { findRenderedDOMComponentWithClass } from "react-dom/test-utils";
 
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/database");
 
-function Home(props){
-    return(
-        <div>
-            <Navigation/>
-            Hola mundo
-        </div>
-    )
+function Home(props) {
+  const [allCases, setAllCases] = useState([]);
+  var dates = [];
+  const [positivos, setPositivos] = useState([]);
+  const [negativos, setNegativos] = useState([]);
+  const [fallecido, setFallecido] = useState([]);
+  const [curado, setCurado] = useState([]);
+  const [UCI, setUCI] = useState([]);
+  const [casa, setCasa] = useState([]);
+  const [hospital, setHospital] = useState([]);
+  var unique = [];
+  var date_counter_array = [];
+  var [xy, setXy] = useState([]);
+
+  let zip = (a1, a2) => a1.map((x, i) => [x, a2[i]]);
+
+  useEffect(() => {
+    getAllCases();
+  }, []);
+  function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+
+  const getAllCases = () => {
+    firebase
+      .database()
+      .ref("/casos/")
+      .on("value", (snapshot) => {
+        var allFBData = _.toArray(snapshot.val());
+        setAllCases(allFBData);
+      });
+  };
+
+  const handleSubmit1 = () => {
+    //PIE - Casos Negativos/positivos
+
+    var contadorP = 0;
+    var contadorN = 0;
+    for (var i = 1; i < allCases.length; i++) {
+      var casosparcial = Object.values(allCases[i]);
+
+      if (casosparcial[10] == "Positivo") {
+        contadorP = contadorP + 1;
+      } else if (casosparcial[10] == "Negativo") {
+        contadorN = contadorN + 1;
+      }
+    }
+    setPositivos(contadorP);
+    setNegativos(contadorN);
+    console.log(allCases);
+
+    var date_counter_array = [];
+
+    for (var i = 1; i < allCases.length; i++) {
+      var casosparcial = Object.values(allCases[i]);
+      dates.push(casosparcial[6]);
+    }
+    unique = dates.filter(onlyUnique);
+
+    console.log(unique);
+    var date_counter_array_aux = new Array(unique.length).fill(0);
+
+    for (var i = 1; i < allCases.length; i++) {
+      var casosparcial = Object.values(allCases[i]);
+
+      for (var j = 0; j < unique.length; j++) {
+        if (casosparcial[10] == "Positivo" && casosparcial[6] == unique[j]) {
+          date_counter_array_aux[j] = date_counter_array_aux[j] + 1;
+          date_counter_array[j] = date_counter_array_aux[j];
+        } else if (
+          casosparcial[10] == "Negativo" &&
+          casosparcial[6] == unique[j]
+        ) {
+          date_counter_array[j] = date_counter_array_aux[j];
+        }
+      }
+    }
+    unique = ["Dias"].concat(unique);
+    date_counter_array = ["Casos"].concat(date_counter_array);
+
+    setXy(zip(unique, date_counter_array));
+
+    var contadorMuer = 0;
+    var contadorCur = 0;
+    for (var i = 1; i < allCases.length; i++) {
+      var casosparcial = Object.values(allCases[i]);
+
+      if (casosparcial[5] == "Curado") {
+        contadorCur = contadorCur + 1;
+      } else if (casosparcial[5] == "Fallecido") {
+        contadorMuer = contadorMuer + 1;
+      }
     }
 
-export default withRouter(Home)
+    setFallecido(contadorMuer);
+    setCurado(contadorCur);
+
+    var contadorUCI = 0;
+    var contadorCasa = 0;
+    var contadorHos = 0;
+
+    for (var i = 1; i < allCases.length; i++) {
+      var casosparcial = Object.values(allCases[i]);
+
+      if (casosparcial[5] == "Positivo en casa") {
+        contadorCasa = contadorCasa + 1;
+      } else if (casosparcial[5] == "Positivo en hospital") {
+        contadorHos = contadorHos + 1;
+      } else if (casosparcial[5] == "Positivo en UCI") {
+        contadorUCI = contadorUCI + 1;
+      }
+    }
+
+    setCasa(contadorCasa);
+    setUCI(contadorUCI);
+    setHospital(contadorHos);
+    console.log(positivos);
+  };
+  console.log(xy);
+  return (
+    <div>
+      <Navigation />
+      <Chart
+        width={"500px"}
+        height={"300px"}
+        chartType="PieChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ["Casos", "Negativo/Positivo"],
+          ["Casos Positivos", positivos],
+          ["Casos Negativos", negativos],
+        ]}
+        options={{
+          title: "Casos Positivos / Negativos",
+        }}
+        rootProps={{ "data-testid": "1" }}
+      />
+
+      <Chart
+        width={"500px"}
+        height={"300px"}
+        chartType="PieChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ["Casos", "Infectado/Fallecido/Curado"],
+          ["Infectados", positivos],
+          ["Fallecidos", fallecido],
+          ["Curado", curado],
+        ]}
+        options={{
+          title: "Casos Totales Registrados",
+        }}
+        rootProps={{ "data-testid": "1" }}
+      />
+
+      <Chart
+        width={"500px"}
+        height={"300px"}
+        chartType="PieChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ["Casos", "Infectados"],
+          ["Fallecidos", positivos],
+          ["Positivo en UCI", UCI],
+          ["Positivo en casa", casa],
+          ["Positivo en hospital", hospital],
+        ]}
+        options={{
+          title: "Situació Infectados",
+        }}
+        rootProps={{ "data-testid": "1" }}
+      />
+
+      <Chart
+        width={"600px"}
+        height={"400px"}
+        chartType="LineChart"
+        loader={<div>Loading Chart</div>}
+        data={xy}
+        options={{
+          hAxis: {
+            title: "Time",
+          },
+          vAxis: {
+            title: "Popularity",
+          },
+        }}
+      />
+
+      <button
+        style={{
+          height: "4vh",
+          border: "0",
+          borderRadius: "5px",
+          marginTop: "5vh",
+          marginBottom: "5vh",
+          marginLeft: "0vw",
+          background: "#36ad9e",
+          color: "white",
+          width: "9vw",
+          fontFamily: "'Jost', sans-serif",
+          fontSize: "15px",
+          cursor: "pointer",
+        }}
+        onClick={handleSubmit1}
+      >
+        Ir
+      </button>
+    </div>
+  );
+}
+
+export default withRouter(Home);
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,86 +1,85 @@
-import React,{useEffect, useState} from 'react'
-import {GoogleComponent} from 'react-google-location'
-import { Link, withRouter } from 'react-router-dom'
-import firebase from '../../firebase'
+import React, { useEffect, useState } from "react";
+import { GoogleComponent } from "react-google-location";
+import { Link, withRouter } from "react-router-dom";
+import firebase from "../../firebase";
 import * as ROUTES from "../Routes/Routes";
 import "./Map.css";
 import app from "firebase/app";
 import "firebase/auth";
 import Navigation from "../NavBar/Navigation";
 import Geocode from "react-geocode";
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from "google-map-react";
 import _ from "lodash";
 
-
-
-function Mapa (){
-
-  const [allData, setAllData] = useState([])
-  const [lat, setLat] =useState("");
-  const [lng, setLng] = useState("")
-
+function Mapa() {
+  const [allData, setAllData] = useState([]);
+  const [lat, setLat] = useState("");
+  const [lng, setLng] = useState("");
 
   const getAllData = () => {
     return app
-    .database()
-    .ref("/casos")
-    .on("value", snapshot => {
-      const firebaseData = _.toArray(snapshot.val());
-      setAllData(firebaseData); 
-     
-  })}
+      .database()
+      .ref("/casos")
+      .on("value", (snapshot) => {
+        const firebaseData = _.toArray(snapshot.val());
+        setAllData(firebaseData);
+      });
+  };
 
   useEffect(() => {
-    getAllData();    
-  
+    getAllData();
   }, []);
 
+  useEffect(() => {}, [allData]);
+
   useEffect(() => {
-  
-  }, [allData]);
-
-
-  
-    useEffect(()=>{
-    Geocode.setApiKey("AIzaSyBXFpz69eQZ_N1SHO37O1e7mMmAlkWIikc")
+    Geocode.setApiKey("AIzaSyBXFpz69eQZ_N1SHO37O1e7mMmAlkWIikc");
 
     Geocode.fromAddress("Carrera 42f #90-85").then(
-     
-        response => {
-          const { lat, lng } = response.results[0].geometry.location;
-          console.log(lat, lng);
-          setLat(lat)
-          setLng(lng)
+      (response) => {
+        const { lat, lng } = response.results[0].geometry.location;
+        console.log(lat, lng);
+        setLat(lat);
+        setLng(lng);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }, []);
 
-        },
-        error => {
-          console.error(error);
-        }
-      );
-    },[]);
-
-    
-    return (
-      
-      <div >
-        <Navigation/>
-        <div style={{ marginLeft:"15vw", marginTop:"5vh",marginBottom:"5vh",height: '80vh', width: '70%' }}>
+  return (
+    <div>
+      <Navigation />
+      <div
+        style={{
+          marginLeft: "15vw",
+          marginTop: "5vh",
+          marginBottom: "5vh",
+          height: "80vh",
+          width: "70%",
+        }}
+      >
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAps7iV33s_Nk0RwrOpQDzKw8CrJmgKJkk"}}
-          defaultCenter={{lat:10.9878, lng:-74.7889} }
+          bootstrapURLKeys={{ key: "AIzaSyAps7iV33s_Nk0RwrOpQDzKw8CrJmgKJkk" }}
+          defaultCenter={{ lat: 10.9878, lng: -74.7889 }}
           defaultZoom={11}
-          
         >
-
+          <Marker
+            key={allData.id}
+            position={{
+              lat: 3,
+              lng: 4,
+            }}
+          />
         </GoogleMapReact>
-        </div>
+      </div>
     </div>
+  );
+}
 
-    )
-  } 
-
-  const Marker = props => {
-    return <div className="SuperAwesomePin"></div>
-  }
+const Marker = (props) => {
+  return <div className="SuperAwesomePin"></div>;
+};
 
 export default Mapa;
