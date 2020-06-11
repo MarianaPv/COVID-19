084cfc59ba7210140578979ea35dbf38294cf123
Mariana <marianapertuz@uninorte.edu.co>
2020-06-10 23:55:39 -0500

    Updates

---
+++ precache-manifest.379a365ddee3c8edefcb3ffb3d16c22d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57db15298e71719e2de53d346c365e61",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "53fa8970e335aadfdbc0",
+    "url": "/COVID-19/static/css/2.34309fd7.chunk.css"
+  },
+  {
+    "revision": "3e4638dfea692d25d77d",
+    "url": "/COVID-19/static/css/main.d999816b.chunk.css"
+  },
+  {
+    "revision": "53fa8970e335aadfdbc0",
+    "url": "/COVID-19/static/js/2.44cc8a84.chunk.js"
+  },
+  {
+    "revision": "a0c2f15122d5df84dbd388b3802d6d14",
+    "url": "/COVID-19/static/js/2.44cc8a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4638dfea692d25d77d",
+    "url": "/COVID-19/static/js/main.03bca13b.chunk.js"
+  },
+  {
+    "revision": "ad95497ba3d159764fda",
+    "url": "/COVID-19/static/js/runtime-main.1b179d68.js"
+  },
+  {
+    "revision": "0d7074a3afeeda7498d9486cab925cc7",
+    "url": "/COVID-19/static/media/coronaback.0d7074a3.jpg"
+  },
+  {
+    "revision": "32aefb53b36359f2c23a86ad81613f6f",
+    "url": "/COVID-19/static/media/green.32aefb53.png"
+  },
+  {
+    "revision": "3fbf5a5beb451ec72f18737b28b6730a",
+    "url": "/COVID-19/static/media/pink.3fbf5a5b.png"
+  },
+  {
+    "revision": "8d20cc46c00bf0f5d4bf6c469e53106d",
+    "url": "/COVID-19/static/media/red.8d20cc46.png"
+  },
+  {
+    "revision": "20afb9ce069956da4a6080a85fdcc018",
+    "url": "/COVID-19/static/media/work.20afb9ce.png"
+  },
+  {
+    "revision": "5e27811cad311383714bccd59a5ae18c",
+    "url": "/COVID-19/static/media/yellow.5e27811c.png"
+  }
+]);
--- src/Components/Routes/Routes.js
+++
@@ -1,8 +0,0 @@
-export const HOMEPAGE = "/";
-export const HOME = "/home";
-export const REGISTRO = "/registro";
-export const REGISTROC = "/registro-casos";
-export const CASOS = "/casos";
-export const MAPA = "/mapa-casos";
-export const BUSQUEDA = "/busqueda-casos";
-export const USUARIO = "/registro-usuarios";
